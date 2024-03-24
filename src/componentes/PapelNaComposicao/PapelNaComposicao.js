0367c1f7de931bb89550205c47d891176c727777
Adriano Dias <adriano.dsm1000@gmail.com>
2024-03-24 19:51:28 -0300

    finalizando opacidade da imagem de fundo

--- src/componentes/PapelNaComposicao/PapelNaComposicao.js
+++ src/componentes/PapelNaComposicao/PapelNaComposicao.js
@@ -3,10 +3,13 @@ import './PapelNaComposicao.css'
 import hexToRgba from 'hex-to-rgba'
 
 const PapelNaComposicao = (props) => {
+    const backgroundImageWithOpacity = (imageUrl, opacity) => {
+        return `linear-gradient(rgba(0, 0, 0, ${opacity}), rgba(0, 0, 0, ${opacity})), url(${imageUrl})`;
+    };
     return (
-        (props.jogadores.length > 0) ? <section className='papelNaComposicao' style={{backgroundImage:'url("/imagens/dice-background-img.png")', backgroundSize: '500px 300px', backgroundColor: hexToRgba(props.corPrimaria,'0.6')}}>
+        (props.jogadores.length > 0) ? <section className='papelNaComposicao' style={{backgroundImage:backgroundImageWithOpacity("/imagens/dice-background-img.png", 0.4), backgroundSize: '500px 300px', backgroundColor: hexToRgba(props.corPrimaria,'0.6')}}>
             <input onChange={e => props.mudarCor(e.target.value, props.id)} value={props.corPrimaria} type='color' className='input-cor'/>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <h3 style={{backgroundColor:props.corPrimaria, borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='jogadores'>
                 {props.jogadores.map(jogador => <Jogador corDeFundo={props.corPrimaria} id={jogador.id} nome={jogador.nome} classe={jogador.classe} subclasse={jogador.subclasse} imagem={jogador.imagem} aoDeletar={props.aoDeletar}/>)}
             </div>
